b12569198160c4c0554fe969f29190addf3bfbc5
norjira <norjira@gmail.com>
2021-09-21 11:39:36 +0700

    Add datacontext

--- Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace NewCore.Data
-{
-    public class Class1
-    {
-    }
-}
---
+++ NewCoreDataContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace NewCore.Data
+{
+    public class NewCoreDataContext : DbContext
+    {
+        public NewCoreDataContext(DbContextOptions<NewCoreDataContext> options) : base(options)
+        {
+        }
+    }
+}
