9a538957d01c5472d507a964caefac830999ad23
norjira <norjira@gmail.com>
2021-09-22 08:00:37 +0700

    Update 21/9/21

---
+++ Models/Customer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace NewCore.Data.Models
+{
+    public partial class Customer
+    {
+        public int CustomerId { get; set; }
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public string Email { get; set; }
+    }
+}
